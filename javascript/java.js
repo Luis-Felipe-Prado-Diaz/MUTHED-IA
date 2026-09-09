693ccdc2ff281a1f9221ccb2c93d0780e300543a
Luis-Felipe-Prado-Diaz <felipeprado901@gmail.com>
2026-09-09 08:03:02 +0200

    cambios

--- javascript/java.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector('.menu');
-    const nav = document.querySelector('header .menu');
-
-    menuToggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
-   
-});
